6091494015ba9049eea6721d61cdda7be8f3a221
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-11-02 15:17:39 +0100

    Adds twilio to the mix

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || 'mongodb://localhost/tilnefna'
+  DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || 'mongodb://localhost/tilnefna',
+  ACCOUNT_SID: process.env.ACCOUNT_SID || '198765432',
+  ACCOUNT_MOBILE: process.env.ACCOUNT_MOBILE || '982736353',
+  AUTH_TOKEN: process.env.AUTH_TOKEN || '7363738383',
+  ADMINISTRATOR_MOBILE: process.env.ADMINISTRATOR_MOBILE || '198765432'
 }
