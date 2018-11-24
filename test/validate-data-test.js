67c52d448a8d470d28cdd030db5152effafe9511
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-11-24 16:43:17 +0100

    Implements replyToNumber

--- test/validate-data-test.js
+++ test/validate-data-test.js
@@ -12,6 +12,18 @@ test('it returns true for valid data', t => {
   t.is(result.error, null, 'Valid data ok')
 })
 
+test('valid data might contain replyToNumber', t => {
+  const data = {
+    name: 'Zrrrzzt',
+    mobile: '12345678',
+    nomineeName: 'Maccyber',
+    nomineeMobile: '87654321',
+    replyToNumber: '12345678'
+  }
+  const result = validateData(data)
+  t.is(result.error, null, 'replyToNumber ok')
+})
+
 test('it returns false for invalid data', t => {
   const data = {
     name: 'Zrrrzzt',
@@ -19,4 +31,16 @@ test('it returns false for invalid data', t => {
   }
   const result = validateData(data)
   t.is(result.error.message, 'child "nomineeName" fails because ["nomineeName" is required]', 'Invalid data ok')
+})
+
+test('replyToNumber must be number', t => {
+  const data = {
+    name: 'Zrrrzzt',
+    mobile: '12345678',
+    nomineeName: 'Maccyber',
+    nomineeMobile: '87654321',
+    replyToNumber: 'absc1625'
+  }
+  const result = validateData(data)
+  t.is(result.error.message, 'child "replyToNumber" fails because ["replyToNumber" must be a number]', 'replyToNumber validation ok')
 })
